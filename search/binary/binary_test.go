93065f9a6363a9142d14c54f53f1d0e82b1be334
mrbardia72 <bardia.backend@gmail.com>
2024-03-06 15:25:42 +0330

    faet: add test  binary alg

--- search/binary/binary_test.go
+++ search/binary/binary_test.go
@@ -8,22 +8,30 @@ func TestBinary(t *testing.T) {
 		target int
 	}
 	tests := []struct {
-		name    string
-		args    args
-		want    int
-		wantErr bool
+		name string
+		args args
+		want int
 	}{
 		{
-			// TODO: Add test cases.
+			name: "found target",
+			args: args{
+				arr:    []int{1, 3, 5, 7, 9, 11},
+				target: 7,
+			},
+			want: 3,
+		},
+		{
+			name: "not found target",
+			args: args{
+				arr:    []int{1, 3, 5, 7, 9, 11},
+				target: 17,
+			},
+			want: -1,
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			got, err := Binary(tt.args.arr, tt.args.target)
-			if (err != nil) != tt.wantErr {
-				t.Errorf("Binary() error = %v, wantErr %v", err, tt.wantErr)
-				return
-			}
+			got := Binary(tt.args.arr, tt.args.target)
 			if got != tt.want {
 				t.Errorf("Binary() got = %v, want %v", got, tt.want)
 			}
